b2731154dd03e621848c74f14b45e656aafa53ea
pickycy <260393979@qq.com>
2019-12-24 15:22:15 +0800

    extract github from code to properties

---
+++ src/main/java/life/majiang/community/dto/GithubUser.java
@@ -0,0 +1,34 @@
+package life.majiang.community.dto;
+
+/**
+ * Created by ACER on 2019/12/24.
+ */
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
