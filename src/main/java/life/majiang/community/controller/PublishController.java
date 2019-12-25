34be60e4dcd05f6ee4511c8d13d276faa541acc7
pickycy <260393979@qq.com>
2019-12-25 16:37:38 +0800

    add publish.html

---
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -0,0 +1,15 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by ACER on 2019/12/25.
+ */
+@Controller
+public class PublishController {
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+}
