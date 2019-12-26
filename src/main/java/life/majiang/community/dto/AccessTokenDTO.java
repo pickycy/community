c0c56152e48db7add6a0ab0b5b02fc33f8698ac7
pickycy <260393979@qq.com>
2019-12-26 18:03:33 +0800

    add avatar_url

--- src/main/java/life/majiang/community/dto/AccessTokenDTO.java
+++ src/main/java/life/majiang/community/dto/AccessTokenDTO.java
@@ -1,52 +1,15 @@
 package life.majiang.community.dto;
 
+import lombok.Data;
+
 /**
  * Created by ACER on 2019/12/24.
  */
+@Data
 public class AccessTokenDTO {
     private String client_id;
     private String client_secret;
     private String code;
     private String redirect_url;
     private String state;
-
-    public String getClient_id() {
-        return client_id;
-    }
-
-    public void setClient_id(String client_id) {
-        this.client_id = client_id;
-    }
-
-    public String getClient_secret() {
-        return client_secret;
-    }
-
-    public void setClient_secret(String client_secret) {
-        this.client_secret = client_secret;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getRedirect_url() {
-        return redirect_url;
-    }
-
-    public void setRedirect_url(String redirect) {
-        this.redirect_url = redirect;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
 }
